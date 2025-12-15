e14265bd851cddac1152632072b0496fafe5b416
tuandao-zenai <tuandq.zenai@gmail.com>
2025-12-15 23:05:56 +0700

    update

--- test.py
+++ test.py
@@ -4,4 +4,6 @@ url = "http://localhost:10006/generate"
 
 response = requests.post(url, files={"prompt_image_file": open("cr7.png", "rb")})
 
-print(response.json())
+# save the response content to ply file
+with open("response.ply", "wb") as f:
+    f.write(response.content)
